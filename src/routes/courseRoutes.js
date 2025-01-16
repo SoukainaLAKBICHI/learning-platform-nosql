9b1c467046158de1412c878022b1481cd431e38a
SoukainaLAKBICHI <soukaina.lakbichi-etu@etu.univh2c.ma>
2025-01-16 13:10:42 +0100

    Modifier Les routes

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -1,7 +1,7 @@
 // Question: Pourquoi séparer les routes dans différents fichiers ?
-// Réponse : 
+// Réponse : Séparer les routes dans différents fichiers améliore l'organisation, facilite la lecture du code, et permet une maintenance plus simple en structurant les fonctionnalités par module ou domaine.
 // Question : Comment organiser les routes de manière cohérente ?
-// Réponse: 
+// Réponse: Pour organiser les routes de manière cohérente, regroupez-les par fonctionnalité ou domaine dans des fichiers distincts, utilisez des noms explicites pour les fichiers, et centralisez leur importation dans un routeur principal.
 
 const express = require('express');
 const router = express.Router();
@@ -9,7 +9,8 @@ const courseController = require('../controllers/courseController');
 
 // Routes pour les cours
 router.post('/', courseController.createCourse);
+router.get('/', courseController.getAllCourses);
 router.get('/:id', courseController.getCourse);
-router.get('/stats', courseController.getCourseStats);
+router.delete('/:id', courseController.deleteCourse);
 
 module.exports = router;
